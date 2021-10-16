0551e2feea07fe6a4a993207deea3b377a7d0e3c
Shakhzodbek <shakhzodbek96@mail.ru>
2021-10-16 17:35:09 +0500

    Company and Branch migration, model, factory added

---
+++ app/Models/Branch.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Http\Traits\HasFilters;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Branch extends Model
+{
+    use HasFactory,HasFilters;
+    protected $fillable = [
+        'company_id',
+        'name',
+        'address',
+        'phone'
+    ];
+}

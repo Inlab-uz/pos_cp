e6e0ff2992e8f9973237f7df1b02396cf773e6c3
itunisoftgroup <itunisoftgroup@gmail.com>
2021-10-17 10:50:30 +0500

    Barcode look up Service finished

--- app/Services/Barcode/GlobalService.php
+++ app/Services/Barcode/GlobalService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services\Barcode;
 
+use App\Models\BarcodeAPI\Product;
+
 class GlobalService
 {
 
@@ -9,6 +11,11 @@ class GlobalService
     public function search($barcode)
     {
 
+        $product = Product::where("barcode_number", $barcode)->first();
+
+        if ($product != null) {
+            return self::productB($barcode);
+        }
 
         $curl = curl_init();
 
@@ -26,17 +33,51 @@ class GlobalService
         $response = curl_exec($curl);
 
         curl_close($curl);
+        $response = json_decode($response, true);
         if ($response == null) {
-            return false;
+            return response()->json(["status" => false]);
         } else {
-            return [
-                "title" => $response["products"][0]["title"],
-                "brand" => $response["products"][0]["brand"],
-                "images" => $response["products"][0]["images"],
 
-            ];
+            $products = $response["products"];
+            if (count($products) > 0)
+                foreach ($products as $p) {
+                    $product = new Product();
+                    $product->barcode_number = $p["barcode_number"];
+                    $product->barcode_formats = $p["barcode_formats"];
+                    $product->title = $p["title"];
+                    $product->brand = $p["brand"];
+                    $product->model = $p["model"];
+                    $product->manufacturer = $p["manufacturer"];
+                    $product->category = $p["category"];
+                    $product->description = $p["description"];
+                    $product->images = json_encode($p["images"]);
+                    $product->ingredients = $p["ingredients"];
+                    $product->age_group = $p["age_group"];
+                    $product->nutrition_facts = $p["nutrition_facts"];
+                    $product->energy_efficiency_class = $p["energy_efficiency_class"];
+                    $product->color = $p["color"];
+                    $product->gender = $p["gender"];
+                    $product->material = $p["material"];
+                    $product->pattern = $p["pattern"];
+                    $product->format = $p["format"];
+                    $product->multipack = $p["multipack"];
+                    $product->size = $p["size"];
+                    $product->length = $p["length"];
+                    $product->width = $p["width"];
+                    $product->height = $p["height"];
+                    $product->weight = $p["weight"];
+                    $product->save();
+                }
+
         }
+        return self::productB($barcode);
 
 
+    }
+
+    static function productB($barcode)
+    {
+        $products = Product::where("barcode_number", $barcode)->get();
+        return response()->json(["products" => $products]);
     }
 }

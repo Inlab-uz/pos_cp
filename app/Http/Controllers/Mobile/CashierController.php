8b7c7ffa4e7c8ab4ac87affcaf4acc4a65a97e75
abdulloh1105 <abdullajon.28057@gmail.com>
2021-10-22 09:10:31 +0500

    manager and cashier add

--- app/Http/Controllers/Mobile/CashierController.php
+++ app/Http/Controllers/Mobile/CashierController.php
@@ -3,15 +3,28 @@
 namespace App\Http\Controllers\Mobile;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cashier;
+use App\Models\Category;
 use App\Models\Import;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
-class CashierController extends Controller
+class CashierController extends MobileResponseController
 {
     public function search(Request $request){
-
+        $cashier = Cashier::find(auth()->user()->id);
+        $categories = Category::where('branch_id', $cashier->branch_id)->get();
+        if ($categories){
+            foreach ($categories as $category){
+                $product = Product::where('barcode_number', $request->barcode_number)->where('category_id', $category->id)->first();
+            }
+            if ($product){
+                return $this->success($product);
+            }
+            return $this->error('Mahsulot topilmadi!');
+        }
+        return $this->error('Mahsulot topilmadi!');
     }
     public function orderCreate(Request $request){
 
---
+++ database/migrations/2021_10_22_084217_create_cashiers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCashiersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cashiers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->integer('company_id')->index();
+            $table->integer('branch_id')->index();
+            $table->integer('manager_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cashiers');
+    }
+}
